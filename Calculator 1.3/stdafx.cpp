edd6fd4bca721657362468d22fd94d131a8c3174
d1agnoz <klepoz.672@mail.ru>
2020-10-02 00:13:23 +0300

    2019 update

--- Calculator 1.3/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Calculator 1.3.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
